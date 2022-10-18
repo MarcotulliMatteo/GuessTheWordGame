b23ec74b035bf0a4847d2a7e50c0a97a416f506d
MarcotulliMatteo <marcotullimatteo5b@gmail.com>
2022-10-18 13:09:44 +0200

    fix: restart game

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ const Header = (props) => {
     return (
         <div className="header">
             <h2>Guess The Word!</h2>
-            <h3 className="reset" onClick={() => props.restartGame()}>Reset Game</h3>
+            <h3 className="reset" onClick={() => props.restartGame()}>Restart Game</h3>
         </div>
     )
 }
